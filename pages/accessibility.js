f50d575f2139ef36b3bce30b4003375f0754c66f
Nirbhay <nirbhayrana8@gmail.com>
2022-10-08 17:21:42 +0530

    fixed contact us and added accessibility page

---
+++ pages/accessibility.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import styles from "../styles/Accessibility.module.css"
+
+export default function Accessibility() {
+	return (
+		<>
+		<div className={styles.mainContainer}>
+		  <Navbar isDarkTheme={true}/>
+		  <h1 className={styles.contactComponentTitle}></h1>
+			<div className={styles.contentRoot}>
+				<div className={styles.contentCentered}>
+					<p>Our mission has always been to make travel more meaningful and accessible. As such, we strive to make it as easy as possible for those with vision, hearing, or other disabilities to learn about our property, reserve their stay with us and enjoy all that our property has to offer.</p>
+					<p>If you have questions or would like to report an issue relating to the accessibility of this website, or if you have difficulty using our website because of a covered disability, please contact us at the telephone number(s) and/or email address listed below.</p>
+					<ul>
+						<li><a href="tel:+919459201765">+919459201765</a></li>
+						<li><a href="mailto:nirbhayrana8@gmail.com">nirbhayrana8@gmail.com</a></li>
+					</ul>
+				</div>
+			</div>
+			<div className={styles.footerContainer}>
+			  <Footer />
+			</div>
+		</div>
+	  </>
+	  )
+}
+
+Accessibility.getLayout = (page) => {
+	return page;
+}
--- pages/contact.js
+++ pages/contact.js
@@ -13,11 +13,21 @@ export default function Contact() {
       <h1 className={styles.contactComponentTitle}></h1>
       <div className={styles.contactHeader}>
         <h2>Contact us today!</h2>
+        <p>Let&apos;s make Thatharna your next getaway by contacting us and ironing out the details.</p>
       </div>
-      <div className={styles.content}>
-        <p>By now we sure have you interested in booking the best stay available at Thatharna. You can call us at <ins>9459201765</ins> or email us <a href="mailto:nirbhayrana8@gmail.com">here</a></p>
-      </div>
-      <Footer></Footer>
+        <div className={styles.content}>
+          <div className={styles.tile}>
+            <i class="fa-solid fa-envelope"></i>
+            <p>Email us at <a href="mailto:nirbhayrana8@gmail.com">nirbhayrana8@gmail.com</a></p>
+          </div>
+          <div className={styles.tile}>
+            <i class="fa-solid fa-phone"></i>
+            <p>Call us at <a href="tel:+919459201765">9459201765</a></p>
+          </div>
+        </div>
+        <div className={styles.footerContainer}>
+          <Footer />
+        </div>
     </div>
   </>
   )
